1d7c5a03d2c502dfb0d004c6d9fe0bb18a212921
livio <livio@stat.unipd.it>
2015-10-08 12:14:46 +0200

    Florian as author

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -2,21 +2,19 @@
 // Generator token: 10BE3573-1514-4C36-9D1C-5A225CD40393
 
 #include <RcppArmadillo.h>
+#include <Rcpp.h>
 
 using namespace Rcpp;
 
+
 // rom
-arma::mat rom(int n);
+mat rom(int n);
 RcppExport SEXP flip_rom(SEXP nSEXP) {
 BEGIN_RCPP
-    SEXP __sexp_result;
-    {
-        Rcpp::RNGScope __rngScope;
-        Rcpp::traits::input_parameter< int >::type n(nSEXP );
-        arma::mat __result = rom(n);
-        PROTECT(__sexp_result = Rcpp::wrap(__result));
-    }
-    UNPROTECT(1);
-    return __sexp_result;
+    Rcpp::RObject __result;
+    Rcpp::RNGScope __rngScope;
+    Rcpp::traits::input_parameter< int >::type n(nSEXP);
+    __result = Rcpp::wrap(rom(n));
+    return __result;
 END_RCPP
 }
